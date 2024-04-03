b9be275715880993c600b4d1c988d662c1bf2186
Kerwin <zhangyu@ifunbow.com>
2024-04-03 14:37:44 +0800

    data

--- app/src/main/java/com/example/banner/model/BannerModel.kt
+++ app/src/main/java/com/example/banner/model/BannerModel.kt
@@ -1,5 +1,16 @@
 package com.example.banner.model
 
 data class BannerModel(
-    val img: Int,
-)
+    var img: Int,
+    var id: String = "MTS-100SY-5098",
+    var pass: String = "PASS:00001111(04:14)",
+    var wifi: String = "192.168.43.1",
+    var lan: String = "192.168.3.61/N/A"
+) {
+
+    fun showId() :String = "ID:$id"
+    fun showPass(): String = "PASS:$pass"
+    fun showWifi(): String = "Wifi Ip:$wifi"
+    fun showLan(): String = "Lan Ip:$lan"
+
+}
